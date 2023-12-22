9a2b8450fac0a70f47c5ce3aee79d29de0c5b4f2
Edison Jeon <edisonthe2nd@gmail.com>
2023-12-21 22:58:45 -0800

    finished mvp, need to review

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,31 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_REMEMBER = "MEMORY_REMEMBER";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
-export const addOne = () => {
-    return({type:ADD_ONE});
-}
-
-export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+export default {
+  addOne: () => {
+    return { type: ADD_ONE };
+  },
+  applyNumber: (number) => {
+    return { type: APPLY_NUMBER, payload: number };
+  },
+  changeOperation: (operator) => {
+    return { type: CHANGE_OPERATION, payload: operator };
+  },
+  clearDisplay: () => {
+    return { type: CLEAR_DISPLAY };
+  },
+  memoryAdd: () => {
+    return { type: MEMORY_ADD };
+  },
+  memoryRemember: () => {
+    return { type: MEMORY_REMEMBER };
+  },
+  memoryClear: () => {
+    return { type: MEMORY_CLEAR };
+  },
+};
